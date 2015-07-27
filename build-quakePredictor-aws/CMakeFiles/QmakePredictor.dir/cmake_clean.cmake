6e913be2b5245cdc361c6bc8930612d41b14cb78
zeryx <1892175+zeryx@users.noreply.github.com>
2015-07-27 12:43:05 -0300

    test

---
+++ build-quakePredictor-aws/CMakeFiles/QmakePredictor.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QmakePredictor.dir/main.cpp.o"
+  "QmakePredictor.pdb"
+  "QmakePredictor"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/QmakePredictor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
