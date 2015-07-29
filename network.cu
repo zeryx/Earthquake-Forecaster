d2b3186bb1db60aea58fa6080745348a25da7158
zeryx <1892175+zeryx@users.noreply.github.com>
2015-07-29 13:44:07 -0300

    more testing with cuda, populating random individuals.

--- network.cu
+++ network.cu
@@ -6,18 +6,32 @@
 #include <thrust/iterator/counting_iterator.h>
 #include <thrust/system_error.h>
 #include <cuda.h>
+#include <vector>
 #include <cuda_runtime_api.h>
 
-struct genRand{
- __host__ __device__
- float operator()(const unsigned int n) const{
-    double result;
-    thrust::default_random_engine randEng;
-    thrust::uniform_real_distribution<float> uniDist(0,1);
-    randEng.discard(n);
-    result = uniDist(randEng);
-    return result;
-}};
+struct genRand: thrust::unary_function<Individual, int>{
+
+    int numWeights;
+
+    genRand(int _numWeights) : numWeights(_numWeights){}
+
+    __host__ __device__
+    Individual operator()(Individual n) const{
+        unsigned int idx= threadIdx.x*blockDim.x;
+
+        n._weights = new float[numWeights];
+
+        for(int i=0; i<numWeights; i++){
+            idx = idx +i;
+            thrust::default_random_engine randEng;
+            thrust::uniform_real_distribution<float> uniDist(0,1);
+            randEng.discard(idx);
+            n._weights[i] =  uniDist(randEng);
+        }
+        return n;
+    }};
+
+
 NetworkGenetic::NetworkGenetic(){}
 
 NetworkGenetic::NetworkGenetic(const int &numInNeurons, const int &numHiddenNeurons,
@@ -33,21 +47,28 @@ bool NetworkGenetic::generatePop(int popsize){
     cudaEvent_t start, stop;
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
+    thrust::device_vector<Individual> testing;
+    testing.resize(popsize);
 
-    thrust::device_vector<float> testing(popsize);
     try{
         cudaEventRecord(start);
-        thrust::transform(thrust::counting_iterator<int>(0),
-                          thrust::counting_iterator<int>(popsize), testing.begin(), genRand());
+        thrust::transform(testing.begin(),
+                          testing.end(), testing.begin(), genRand(_neuronsTotalNum));
     }
     catch(thrust::system_error &err){
         std::cerr<<"error transforming: "<<err.what()<<std::endl;
         return false;
     }
-cudaEventRecord(stop);
-float miliseconds = 0;
+    cudaEventRecord(stop);
+    float miliseconds = 0;
+    std::vector<Individual> printer(testing.size());
     for(int i=0; i<popsize; i++){
-        std::cout<<testing[i]<<std::endl;
+        thrust::copy(testing.begin(), testing.end(),printer.begin());
+        }
+    for(int i=0; i<popsize; i++){
+        for(int k=0; k<_neuronsTotalNum; k++){
+            std::cout<< printer[k]._weights[k]<<std::endl;
+        }
     }
     cudaEventElapsedTime(&miliseconds, start, stop);
 
--- network.h
+++ network.h
@@ -4,6 +4,12 @@
 #include <thrust/device_vector.h>
 #include <map>
 
+struct Individual{//stores the weights and its fitness values
+public:
+    float* _weights;
+    float _absoluteFitness, _relativeFitness;
+};
+
 class  NetworkGenetic{
 
 public:
@@ -12,6 +18,7 @@ public:
                    const int &numOutNeurons, std::map<const int, int> &connections);
     bool generatePop(int popsize); // tells the network how many individuals you want to start with
 private:
+    thrust::device_vector<Individual> _individuals;
     thrust::host_vector<int> _constantNNParams;
     int _neuronsTotalNum;
     std::map<const int, int> _connections;
