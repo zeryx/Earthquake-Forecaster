993fe8c16534035315bd52a57edb3eb9534682f4
zeryx <1892175+zeryx@users.noreply.github.com>
2015-08-01 11:40:35 -0300

    - added tiny XML functionality for gtf.xml, and SiteInfo.xml document inputs. - accidentally deleted a bunch of work done yesterday, will try to make it up ASAP.

--- network.cu
+++ network.cu
@@ -11,6 +11,23 @@
 #include <cuda_runtime.h>
 #include <tinyxml2.h>
 
+//macros
+//xml error message handling
+#ifndef XMLCheckResult
+#define XMLCheckResult(a_eResult) if (a_eResult != tinyxml2::XML_SUCCESS) { printf("Error: %i\n", a_eResult);  exit(a_eResult); }
+#endif
+//cuda error message handling
+#define CUDA_SAFE_CALL(call)                                          \
+    do {                                                                  \
+    cudaError_t err = call;                                           \
+    if (cudaSuccess != err) {                                         \
+    fprintf (stderr, "Cuda error in file '%s' in line %i : %s.\n",\
+    __FILE__, __LINE__, cudaGetErrorString(err) );       \
+    exit(EXIT_FAILURE);                                           \
+    }                                                                 \
+    } while (0)
+
+
 template <typename T>
 __global__ void genWeights( DataArray<T> ref, long in, int nRegWeights, int indLength){
     long idx = blockDim.x*blockIdx.x + threadIdx.x;
@@ -22,6 +39,9 @@ __global__ void genWeights( DataArray<T> ref, long in, int nRegWeights, int indL
         ref._array[idx*indLength+i] = uniDist(randEng);
     }
 }
+
+
+
 
 NetworkGenetic::NetworkGenetic(const int &numInNeurons, const int &numHiddenNeurons, const int &numMemoryNeurons,
                                const int &numOutNeurons, const int &numHiddenLayers,  const std::map<const int, int> &connections){
@@ -44,13 +64,13 @@ void NetworkGenetic::initializeWeights(){
     int minGridSize; //the minimum grid size needed to achive max occupancy
     int gridSize; // the actual grid size needed
     int individualSize = _NNParams[1]+1;//contains all neruons, plus 1 for fitness vals
-    _initialPopsize = _DGeneticsData.size()/(individualSize);
-    std::cout<<"initial population: "<<_initialPopsize<<std::endl;
+    _NNParams[8] = _DGeneticsData.size()/(individualSize);
+    std::cout<<"initial population: "<<_NNParams[8]<<std::endl;
     long time = std::clock();
     cudaEventRecord(start, 0);
     cudaDeviceSynchronize();
-    CUDA_SAFE_CALL (cudaOccupancyMaxPotentialBlockSize( &minGridSize, &blocksize, (void*)genWeights<double>, 0, _initialPopsize));
-    gridSize = (_initialPopsize + blocksize -1)/blocksize;
+    CUDA_SAFE_CALL (cudaOccupancyMaxPotentialBlockSize( &minGridSize, &blocksize, (void*)genWeights<double>, 0, _NNParams[8]));
+    gridSize = (_NNParams[8] + blocksize -1)/blocksize;
     genWeights<double><<<gridSize, blocksize>>>(convertToKernel<double>(_DGeneticsData), time, _NNParams[2], individualSize);
     cudaDeviceSynchronize();
     float miliseconds = 0;
@@ -64,32 +84,58 @@ void NetworkGenetic::initializeWeights(){
 
 
 void NetworkGenetic::init(std::string siteInfo){
-    int SLEN, sampleRate;
-    std::vector<double> sitesData;
-   tinyxml2::XMLDocument doc;
-   doc.LoadFile(siteInfo.c_str());
-   std::string title = doc.FirstChildElement("Sites")->FirstChildElement("site")->GetText();
-   std::cout<<"title"<<std::endl;
-//    this->_HInitData[0] = doc.first_node()->
-//    this->_HInitData[1] = sampleRate;
-//    for(int i=0; i<SLEN; i++){
-//        this->_HInitData[2+i] = sitesData[i];
-//    }
+    int dataSet, SLEN;
+    tinyxml2::XMLDocument doc;
+    doc.LoadFile(siteInfo.c_str());
+
+    tinyxml2::XMLNode * pRoot = doc.FirstChild();
+    if(pRoot == NULL) exit(tinyxml2::XML_ERROR_FILE_READ_ERROR);
+    tinyxml2::XMLElement * pElement = pRoot->FirstChildElement("Sites");
+    if(pRoot == NULL) exit(tinyxml2::XML_ERROR_PARSING_ELEMENT);
+    tinyxml2::XMLError eResult = pElement->QueryIntAttribute("data_set", &dataSet);
+    XMLCheckResult(eResult);
+
+    eResult = pElement->QueryIntAttribute("num_sites", &SLEN);
+    XMLCheckResult(eResult);
+    _DInitData.push_back(SLEN);
+    _DInitData.push_back(dataSet);
+    tinyxml2::XMLElement *SitesList = pRoot->FirstChildElement("Site");
+
+    while(SitesList != NULL){
+        int sampleData;
+        double longitude, latitude;
+
+        eResult = SitesList->QueryIntAttribute("sample_rate", &sampleData);
+        XMLCheckResult(eResult);
+        _DInitData.push_back(sampleData);
+        eResult = SitesList->QueryDoubleAttribute("latitude", &latitude);
+        XMLCheckResult(eResult);
+        _DSitesData.push_back(latitude);
+        eResult = SitesList->QueryDoubleAttribute("longitude", &longitude);
+        XMLCheckResult(eResult);
+        _DSitesData.push_back(longitude);
+        SitesList = SitesList->NextSiblingElement("Site");
+    }
+
+
+
 }
 
 
 void NetworkGenetic::allocateHostAndGPUObjects(unsigned int hostMemory, unsigned int deviceMemory,
                                                std::map<const std::string, float> pHostRam,  std::map<const std::string, float> pDeviceRam){
-    unsigned int hostInitDataAlloc = (9*2+2)/sizeof(double); //sampleRate, number of sites, site lon/lat data.
     unsigned int hostGeneticsAlloc = hostMemory*pHostRam.at("genetics")/sizeof(double); //since these are doubles, divide bytes by 8
     unsigned int hostTrainingAlloc = hostMemory*pHostRam.at("input & training")/(sizeof(double)+2);//half for training, half for input I think?
     unsigned int hostInputsAlloc = hostMemory*pHostRam.at("input & training")/(sizeof(float)+2); // their either floats or ints, same amount of bytes.
+    unsigned int DeviceInitDataAlloc = (9*2+2)/sizeof(double); //sample rates, number of sites, and dataset #
+    unsigned int DeviceSitesDataAlloc = (9*2)/sizeof(double); //contains longitudes/latitudes of each site
     unsigned int deviceGeneticsAlloc = deviceMemory*pDeviceRam.at("genetics")/sizeof(double);
     unsigned int deviceTrainingAlloc = deviceMemory*pDeviceRam.at("input & training")/(sizeof(double)+2);
     unsigned int deviceInputsAlloc = deviceMemory*pDeviceRam.at("input & training")/(sizeof(double)+2);
     unsigned int devicePMAIAlloc = 2160*80/sizeof(double); //1.4 mb worth of planetary magnetic activity index for all tests, can store outside of container with other constants.
     //initialize all vectors
-    this->_HInitData.resize(hostInitDataAlloc);
+    this->_DSitesData.resize(DeviceSitesDataAlloc);
+    this->_DInitData.resize(DeviceInitDataAlloc);
     this->_HGeneticsData.resize(hostGeneticsAlloc);
     this->_HTrainingData.resize(hostTrainingAlloc);
     this->_HInputData.resize(hostInputsAlloc);
--- network.h
+++ network.h
@@ -7,15 +7,6 @@
 #include <string>
 #include <cuda.h>
 #include <cuda_runtime_api.h>
-#define CUDA_SAFE_CALL(call)                                          \
-do {                                                                  \
-    cudaError_t err = call;                                           \
-    if (cudaSuccess != err) {                                         \
-        fprintf (stderr, "Cuda error in file '%s' in line %i : %s.\n",\
-                 __FILE__, __LINE__, cudaGetErrorString(err) );       \
-        exit(EXIT_FAILURE);                                           \
-    }                                                                 \
-} while (0)
 class  NetworkGenetic{
 public:
     NetworkGenetic(const int &numInNeurons, const int &numHiddenNeurons, const int &numMemoryNeurons,
@@ -31,12 +22,12 @@ private:
     thrust::device_vector<int> _DInputData; //device loaded memory object containing the site input data.
     thrust::device_vector<double> _DTrainingData; //device loaded memory object containing the teacher data.
     thrust::device_vector<double> _DPMAIndex; //device loaded memory object containing the planetary magnetic activity index. (all of it should fit)
-    thrust::host_vector<int>  _HInitData; //storage of input from Init function call.
+    thrust::device_vector<int>  _DInitData; //storage of input from Init function call.
+    thrust::device_vector<double> _DSitesData;
     thrust::host_vector<double> _HGeneticsData; //overflow of Genetics Data
     thrust::host_vector<int> _HInputData; //overflow of input Data that cannot fit in GPU memory due to resource limitations.
     thrust::host_vector<double> _HTrainingData; //overflow of training data
     std::map<const int, int> _connections;
-    int _initialPopsize;
 };
 
 
