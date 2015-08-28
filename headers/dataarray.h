3d7ec486831e591a968869a995aca6620e815964
zeryx <zeryx>
2015-08-28 00:07:26 +0000

    updated comments and removed unneeded macro.

--- headers/dataarray.h
+++ headers/dataarray.h
@@ -1,8 +1,6 @@
 #ifndef DATAARRAYS_H
 #define DATAARRAYS_H
-#ifndef CUDA_SAFE_CALL
-#define CUDA_SAFE_CALL(call) do{cudaError_t err = call; if (cudaSuccess != err) {fprintf (stderr, "Cuda error in file '%s' in line %i : %s.\n",__FILE__, __LINE__, cudaGetErrorString(err) ); cudaDeviceReset(); exit(EXIT_FAILURE);}} while (0)
-#endif
+//simple struct that contains both a pointer of type T and a size parameter, used within kernels.
 template <typename T>
 struct kernelArray{
     T* array;
