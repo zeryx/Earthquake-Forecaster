7dda68cc20251e50edc53e1be0c0c14ab5373f4e
Ubuntu <ubuntu@ip-172-31-8-143.ec2.internal>
2015-09-15 13:29:37 +0000

    -added run.sh to the repo as rebuilding it every time gets annoying.

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+trap "exit" INT
+java -jar tester.jar -exec "nvprof -o timeline.nvprof ./Earthquake_Forecaster" -folder "../mount/data/" -seed 113 -train
+java -jar tester.jar -exec "nvprof -o timeline.nvprof ./Earthquake_Forecaster" -folder "../mount/data/" -seed 142 -train
+java -jar tester.jar -exec "nvprof -o timeline.nvprof ./Earthquake_Forecaster" -folder "../mount/data/" -seed 129 -train
+java -jar tester.jar -exec "nvprof -o timeline.nvprof ./Earthquake_Forecaster" -folder "../mount/data/" -seed 148 -train
+java -jar tester.jar -exec "nvprof -o timeline.nvprof ./Earthquake_Forecaster" -folder "../mount/data/" -seed 151 -train
+java -jar tester.jar -exec "nvprof -o timeline.nvprof ./Earthquake_Forecaster" -folder "../mount/data/" -seed 156 -train
+java -jar tester.jar -exec "nvprof -o timeline.nvprof ./Earthquake_Forecaster" -folder "../mount/data/" -seed 169 -train
